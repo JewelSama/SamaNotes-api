7cf9229c7e4ba9ab8e28f061140ff70a1364baf9
Jewel Osafile <osafilejewel@gmail.com>
2022-10-01 20:35:13 +0100

    fillables and relationship with Category

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'note',
+    ];
+
+    public function category(){
+        $this->belongsTo(Category::class);
+    }
 }
