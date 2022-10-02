978d395282f00b53ca404b0ecc3dbddcbc612383
Jewel Osafile <osafilejewel@gmail.com>
2022-10-02 14:04:32 +0100

    user_id

--- database/migrations/2022_10_01_193217_create_notes_table.php
+++ database/migrations/2022_10_01_193217_create_notes_table.php
@@ -16,6 +16,7 @@ class CreateNotesTable extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
             $table->string('category');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->longText('note');
             // $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
