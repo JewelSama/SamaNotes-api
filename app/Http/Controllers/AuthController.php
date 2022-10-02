373b9f5290107f8cba0dacc552ab39f38e4b9d45
Jewel Osafile <osafilejewel@gmail.com>
2022-10-02 14:03:37 +0100

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,6 +29,37 @@ class AuthController extends Controller
         return response($response, 201);
     }
 
+    public function login(Request $request){
+        // auth()->user()->tokens()->delete();
+        // return auth()->user()->tokens();
+        
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+        
+        //check email
+        $user = User::where('email', $fields['email'])->first();
+        $user->tokens()->delete();
+        
+
+        //check password
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            $response = [
+                'Bad credentialds'
+            ];
+            return response($response, 401);
+        };
+        
+        $token = $user->createToken('samaToken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+        return response($response, 200);
+    }
+
 
     public function logout(Request $request){
         auth()->user()->tokens()->delete();
